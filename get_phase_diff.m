16993f26cce846f698634898d55cf5f1c0fca26f
ivan-skelin <104469377+ivan-skelin@users.noreply.github.com>
2023-02-25 14:18:16 -0800

    Update and rename get_hilb.m to get_phase_diff.m

---
+++ get_phase_diff.m
@@ -0,0 +1,35 @@
+
+
+
+function [phase_diff] = get_phase_diff(signal1, signal2, freq, srate)
+
+% curr_plv computes the phase differences between the two time series
+%
+% Inputs : 
+%     signal1: a vector representing one time series (e.g. EEG from one electrode)
+%     signal2: a vector representing another time series (e.g. EEG from one electrode)
+%     freq:    a 2-element vector, defining the low and high  bandpass limits
+%     srate:   a scalar, defining the sampling frequency (both signals must be recorded or later processed to have the same sampling frequency)
+%
+% Outputs :
+%     phase_diff: a vector containing the phase differences (in radians) between the two time series at a given frequency, at each timepoint
+      
+nyq_freq = srate/2;
+
+freq_low = freq(1) / nyq_freq;
+freq_high = freq(2) / nyq_freq;
+
+[A,B] = cheby1(3, 0.1, [freq_low,freq_high]);
+
+filt_signal1 = filtfilt(A, B, signal1);
+
+angle_signal1 = angle(hilbert(filt_signal1));
+
+
+filt_signal1 = filtfilt(A, B, signal1);
+
+angle_signal1 = angle(hilbert(filt_signal1));
+
+phase_diff = (exp(1i *(angle_signal1 - angle_signal1)));   %phase difference is in the brackets
+
+
