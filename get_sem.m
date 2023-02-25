09ef19a3835f04bf98f4e98d92f29a84cc0fbe56
ivan-skelin <104469377+ivan-skelin@users.noreply.github.com>
2023-02-25 13:53:50 -0800

    Update get_sem.m

--- get_sem.m
+++ get_sem.m
@@ -1,7 +1,14 @@
 
-function [sem] = get_sem(trials)
-if size(trials)>1
- sem = std(trials)/sqrt(size(trials,1));
+function [sem] = get_sem(values)
+%   get_sem.m calculates the standard error of the mean from the input
+%  Input:
+%   values : a 1D or 2D array
+%  
+%  Output: 
+%   sem : a scalar in the case of 1D input or a vector in the case of 2D input (the SEM is calculated over the first dimension by default).
+
+if size(values)>1
+ sem = std(values)/sqrt(size(values,1));
 else
-     sem = std(trials)/sqrt(numel(trials));
+     sem = std(values)/sqrt(numel(values));
 end
